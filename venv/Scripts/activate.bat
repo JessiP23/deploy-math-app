39e606585bd4ad0eee967c1e99c943f2ec37397c
Jessi Pavia <jessi316866@gmail.com>
2024-01-30 15:07:53 -0500

    changes

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jessi\OneDrive\Documentos\computing\repository\venv
+set VIRTUAL_ENV=C:\Users\jessi\OneDrive\Documentos\computing\web-app\deploy-math-app\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
